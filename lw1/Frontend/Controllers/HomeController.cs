c3e9dcf94c3a1b0624d9f311863ac076dca615a5
Starovoytov Alexey <relz0071@gmail.com>
2018-02-06 08:57:53 +0300

    Реализована передача данных на Backend с приёмом и выводом ответа

--- lw1/Frontend/Controllers/HomeController.cs
+++ lw1/Frontend/Controllers/HomeController.cs
@@ -5,14 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Frontend.Models;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
 
 namespace Frontend.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index(FormModel formModel)
+		public async Task<IActionResult> Index(FormModel formModel)
 		{
-			// TODO: Send formModel.Data to Backend, recieve id and display it for user
+			if (formModel.Data != null)
+			{
+				string url = "http://127.0.0.1:5050/api/values";
+				StringContent stringContent = new StringContent($"{{ \"data\": \"{formModel.Data}\"}}", Encoding.UTF8, "application/json");
+				using (HttpClient httpClient = new HttpClient())
+				{
+					httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+					using (HttpResponseMessage response = await httpClient.PostAsync(url, stringContent))
+					using (HttpContent content = response.Content)
+					{
+						formModel.Id = content.ReadAsStringAsync().Result;
+					}
+				}
+			}
 			return View(formModel);
 		}
 
