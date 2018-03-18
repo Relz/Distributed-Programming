47130190cad868710f380b69fdb7038cdc0caaac
Alexey Starovoitov <alexey.starovoitov@travelline.ru>
2018-03-18 13:38:19 +0300

    Добавлено приложение VowelConsonantRater

---
+++ lw5/src/VowelConsonantRater/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace VowelConsonantRater
+{
+	struct VowelConsonant
+	{
+		public int vowelCount;
+		public int consonantCount;
+	}
+
+	class Program
+	{
+		private static readonly string exchangeName = "vowel-cons-counter";
+
+		static void Main()
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using(var connection = factory.CreateConnection())
+			using(var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+				string queueName = channel.QueueDeclare().QueueName;
+				channel.QueueBind(
+					queue: queueName,
+					exchange: exchangeName,
+					routingKey: ""
+				);
+
+				EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					byte[] body = ea.Body;
+					string[] data = Encoding.UTF8.GetString(body).Split('|');
+					int vowelCount;
+					int consonantCount;
+					if (Int32.TryParse(data[1], out vowelCount) && Int32.TryParse(data[2], out consonantCount)) {
+						RedisHelper.Instance.Set(data[0], CalculateRate(vowelCount, consonantCount));
+					}
+				};
+				channel.BasicConsume(
+					queue: queueName,
+					autoAck: true,
+					consumer: consumer
+				);
+
+				Console.WriteLine(" Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+
+		private static string CalculateRate(int vowelCount, int consonantCount)
+		{
+			return consonantCount == 0 ? "Infinity" : ((double)vowelCount / consonantCount).ToString();
+		}
+	}
+}
---
+++ lw5/src/VowelConsonantRater/RabbitMqHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using StackExchange.Redis;
+
+namespace VowelConsonantRater
+{
+	public sealed class RabbitMqHelper
+	{
+		private static readonly string exchangeName = "vowel-cons-counter";
+		private IModel _channel;
+
+		private RabbitMqHelper()
+		{
+			ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+			IConnection connection = factory.CreateConnection();
+			_channel = connection.CreateModel();
+		}
+
+		public static RabbitMqHelper Instance { get; } = new RabbitMqHelper();
+
+		public void SendMessage(string message)
+		{
+			_channel.BasicPublish(
+				exchange: exchangeName,
+				routingKey: "",
+				basicProperties: null,
+				body: Encoding.UTF8.GetBytes(message)
+			);
+		}
+	}
+}
