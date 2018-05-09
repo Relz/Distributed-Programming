a1d13a0af76712f3cc4e2f8b6b3c6c923085bccf
Starovoytov Alexey <relz0071@gmail.com>
2018-05-09 17:55:54 +0300

    Backed возвращает статус и оценку текста

--- lw8/src/Backend/Controllers/TextDetailsController.cs
+++ lw8/src/Backend/Controllers/TextDetailsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RedisLibrary;
+using ModelLibrary;
 
 namespace Backend.Controllers
 {
@@ -13,19 +14,19 @@ namespace Backend.Controllers
 	{
 		// GET api/text_details/<id>
 		[HttpGet("{id}")]
-		public string Get(string id)
+		public TextDetails Get(string id)
 		{
+			Console.WriteLine($"Get text details for id: {id}");
+			TextDetails result = new TextDetails();
+			result.Id = id;
 			Redis.Instance.SetDatabase(Redis.Instance.CalculateDatabase(id));
 			if (!Redis.Instance.Database.KeyExists($"{ConstantLibrary.Redis.Prefix.Text}{id}"))
 			{
-				return "Invalid id";
+				result.Status = "Invalid text id";
 			}
-			if (!Redis.Instance.Database.KeyExists($"{ConstantLibrary.Redis.Prefix.Rank}{id}"))
-			{
-				return "Hasn't calculated yet";
-			}
-			string result = Redis.Instance.Database.StringGet($"{ConstantLibrary.Redis.Prefix.Rank}{id}");
-			Console.WriteLine($"'{ConstantLibrary.Redis.Prefix.Rank}{id}: {result}' from redis database({Redis.Instance.Database.Database})");
+			result.Status = Redis.Instance.Database.StringGet($"{ConstantLibrary.Redis.Prefix.Status}{id}");
+			result.Rank = Redis.Instance.Database.StringGet($"{ConstantLibrary.Redis.Prefix.Rank}{id}");
+			Console.WriteLine($"'{ConstantLibrary.Redis.Prefix.Rank}{id}: {result.Rank}' from redis database({Redis.Instance.Database.Database})");
 
 			return result;
 		}
