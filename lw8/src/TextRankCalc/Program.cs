f6ca995c06581fd2d7dcf52fa9fd269e7d06363f
Starovoytov Alexey <relz0071@gmail.com>
2018-05-09 17:54:07 +0300

    Перед вычислением оценки текста назначен статус Processing

--- lw8/src/TextRankCalc/Program.cs
+++ lw8/src/TextRankCalc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RabbitMqLibrary;
+using RedisLibrary;
 
 namespace TextRankCalc
 {
@@ -17,7 +18,7 @@ namespace TextRankCalc
 			rabbitMq.ConsumeQueue(textIdAndStatus =>
 			{
 				Console.WriteLine($"New message from {_listeningExchangeName}: \"{textIdAndStatus}\"");
-				
+
 				string[] splittedMessage = textIdAndStatus.Split(ConstantLibrary.RabbitMq.Delimiter);
 				string textId = splittedMessage[0];
 				bool doesTextAllowed = splittedMessage[1] == ConstantLibrary.RabbitMq.ProcessingLimiter.Status.True;
@@ -27,6 +28,11 @@ namespace TextRankCalc
 					Console.WriteLine($"{textId} not allowed");
 					return;
 				}
+				Redis.Instance.SetDatabase(Redis.Instance.CalculateDatabase(textId));
+				Console.WriteLine($"'{ConstantLibrary.Redis.Prefix.Status}{textId}: {ConstantLibrary.Redis.Status.Processing}' to redis database({Redis.Instance.Database.Database})");
+				Redis.Instance.Database.StringSet($"{ConstantLibrary.Redis.Prefix.Status}{textId}", ConstantLibrary.Redis.Status.Processing);
+
+				// System.Threading.Thread.Sleep(5000);
 
 				Console.WriteLine($"{textId} to {_publishExchangeName} exchange");
 				rabbitMq.PublishToExchange(_publishExchangeName, textId);
